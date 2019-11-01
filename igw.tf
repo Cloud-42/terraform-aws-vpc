4576a8b52a32b195771d77e5efd2426df1560541
chris.morgan <chris.morgan@uniquex.com>
2019-11-01 13:17:21 +0000

    correcting v12 syntax for tagging

--- igw.tf
+++ igw.tf
@@ -6,7 +6,7 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name          = var.environment.igw
+    Name          = "${var.environment}.igw"
     Environment   = var.environment
     Description   = "igw"
     Contact       = var.vpc_contact
