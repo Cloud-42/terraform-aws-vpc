7b56cb2b30f5d177eb27ebda3bc7eca63f7c7e86
chris.morgan <chris.morgan@uniquex.com>
2019-11-01 13:18:59 +0000

    correcting v12 syntax for tagging

--- virtual_gateway.tf
+++ virtual_gateway.tf
@@ -5,7 +5,7 @@ resource "aws_vpn_gateway" "main_vgw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name          = var.environment.vgw
+    Name          = "${var.environment}.vgw"
     Environment   = var.environment
     Description   = "vgw"
     Contact       = var.vpc_contact
